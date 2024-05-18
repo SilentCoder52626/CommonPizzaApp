98788a9670928812e52f3000282716f934df94b3
common khadka <commonkhadka@gmail.com>
2024-05-18 18:24:15 +0545

    Add Main welcome page

---
+++ CommonPizzaApp/Pages/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace CommonPizzaApp.Pages;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CommonPizzaApp/Pages/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace CommonPizzaApp.Pages
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+        {
+            await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+        }
+    }
+
+}
